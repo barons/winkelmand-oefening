acebad75cfb1fb321a3a5d8e2550dc29dbcfa4fb
webmaster <webmaster@L05MAC00.local>
2012-10-25 22:00:13 +0200

    oop einde

---
+++ includes/Table.php
@@ -0,0 +1,45 @@
+<?php
+
+
+/**
+ *
+ * @author Xavier Dekeyster
+ */
+class Table {
+    private $_rows;
+    
+    public function __construct() 
+    {
+        $this->_rows = array();
+    }
+    
+    /**
+     * 
+     * @param string $rows
+     */
+    public function append ($rows)
+    {
+        $this->_rows[] = $rows;
+    }
+    
+    public function draw ()
+    {
+        echo '<table border="1">'.PHP_EOL;
+        echo '<pre>';
+        var_dump($this->_rows);die;
+        echo '</pre>';
+            
+            foreach ($this->_rows as $row) {
+                echo '<tr>'.PHP_EOL;
+                    foreach ($row->getCells() as $cell) {
+                        echo '<td>'. $cell->getContent().'</td>'.PHP_EOL;
+                    }
+                echo '</tr>'.PHP_EOL;
+            }
+        
+        echo '</table>'.PHP_EOL;
+        
+    }
+}
+
+?>
